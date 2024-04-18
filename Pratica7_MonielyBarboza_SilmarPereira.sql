4b8c5826399b0df66246c552dd9244d9954d1cb0
Silmar <silmarjr2@gmail.com>
2024-04-17 22:47:46 -0300

    add exercicio 1

--- Pratica7_MonielyBarboza_SilmarPereira.sql
+++ Pratica7_MonielyBarboza_SilmarPereira.sql
@@ -1,3 +1,35 @@
+/* 1. */
+DECLARE
+	CURSOR c_mais_orbitados IS  SELECT e.ID_ESTRELA, e.NOME, count(*) as QTD
+		FROM ORBITA_ESTRELA oe JOIN ESTRELA e ON oe.ORBITADA = e.ID_ESTRELA 
+		GROUP BY e.ID_ESTRELA, e.NOME  
+		HAVING count(*) = (SELECT max(count(*)) FROM ORBITA_ESTRELA GROUP BY ORBITADA);
+
+	v_mais_orbitados c_mais_orbitados%ROWTYPE;
+BEGIN
+	OPEN c_mais_orbitados;
+	FETCH c_mais_orbitados INTO v_mais_orbitados;	
+	
+	IF NOT c_mais_orbitados%FOUND THEN RAISE NO_DATA_FOUND; END if;
+
+	dbms_output.put_line('Quantidade maxima de orbitantes é: ' || v_mais_orbitados.QTD);	
+	dbms_output.put_line('Estrela id: ' || v_mais_orbitados.ID_ESTRELA
+		|| ';  Estrela Nome: ' || v_mais_orbitados.NOME);
+
+	LOOP
+		FETCH c_mais_orbitados INTO v_mais_orbitados;
+		EXIT WHEN c_mais_orbitados%NOTFOUND;
+		dbms_output.put_line('Estrela id: ' || v_mais_orbitados.ID_ESTRELA
+			|| ';  Estrela Nome: ' || v_mais_orbitados.NOME);
+	end LOOP;
+
+	CLOSE c_mais_orbitados;
+
+	EXCEPTION
+		WHEN NO_DATA_FOUND THEN  
+			dbms_output.put_line('Não foram encontrados registros de orbita');
+END;
+
 /* 2. Implemente um programa PL/SQL que remova da base de dados todas as federações que não
 possuem nenhuma nação associada. Imprima a quantidade de federações removidas.
 OBS: use cursor implícito. Faça a remoção usando um único comando DELETE. */

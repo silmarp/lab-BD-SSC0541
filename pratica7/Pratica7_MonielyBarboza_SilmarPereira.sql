554cef6383f72edf9abb79a59ad819d1cf83f17e
Silmar <silmarjr2@gmail.com>
2024-04-29 18:03:00 -0300

    pratica7: add ex3

--- pratica7/Pratica7_MonielyBarboza_SilmarPereira.sql
+++ pratica7/Pratica7_MonielyBarboza_SilmarPereira.sql
@@ -54,4 +54,50 @@ INSERT INTO FEDERACAO VALUES('P7_TESTE2', '17/04/2024');
 INSERT INTO FEDERACAO VALUES('P7_TESTE3', '17/04/2024');
 INSERT INTO FEDERACAO VALUES('P7_TESTE4', '17/04/2024');
 
-       
+
+/*
+3. Implemente um programa PL/SQL que, dado um planeta e uma comunidade (entradas de usuário), 
+inclua a comunidade como habitante do planeta por um período de:  
+• 100 anos a partir da data atual se a comunidade possuir 1000 habitantes ou menos;  
+• 50 anos a partir da data atual se a comunidade possuir mais de 1000 habitantes;  
+Imprima  as  informações  referentes  à  espécie  da  comunidade  (nome,  planeta  de  origem  e  se  é 
+inteligente ou não), e o período (datas) de habitação cadastrado.  
+*/
+
+-- TODO impresão das ultimas informações
+DECLARE
+	v_planeta PLANETA.ID_ASTRO%TYPE;
+	v_comunidade COMUNIDADE.NOME%TYPE;
+	v_especie COMUNIDADE.ESPECIE%TYPE;
+	v_habitantes COMUNIDADE.QTD_HABITANTES%TYPE;
+
+	e_planeta_invalido EXCEPTION;
+	pragma EXCEPTION_INIT(e_planeta_invalido, -2291);
+BEGIN
+    -- entradas do usuario
+	v_planeta := 'Laborum nihil.';
+	v_comunidade := 'Thals';
+	v_especie := 'Kaleds Extermum';
+
+	SELECT QTD_HABITANTES INTO v_habitantes 
+		FROM COMUNIDADE c WHERE c.NOME = v_comunidade AND c.ESPECIE = v_especie;
+	
+	IF v_habitantes > 1000 then
+			-- habitação de 50 anos
+			INSERT INTO HABITACAO values(v_planeta, v_especie, v_comunidade, current_date, add_months(current_date, 600));
+		ELSE
+			-- habitação de 100 anos
+			INSERT INTO HABITACAO values(v_planeta, v_especie, v_comunidade, current_date, add_months(current_date, 1200));
+	end IF;
+
+	COMMIT;
+
+	EXCEPTION
+		WHEN NO_DATA_FOUND THEN  
+			dbms_output.put_line('Não foi possivel encotrar a comunidade');
+		WHEN e_planeta_invalido THEN
+			dbms_output.put_line('O planeta ' || v_planeta || ' é invalido e/ou não existe');
+		WHEN OTHERS  
+       		THEN dbms_output.put_line('Erro nro:  ' || SQLCODE  
+                            || '. Mensagem: ' || SQLERRM );
+END;

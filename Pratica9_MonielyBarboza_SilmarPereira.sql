36959a31e9de2e1b18ee8140d1f7b95096087c05
Silmar <silmarjr2@gmail.com>
2024-05-21 19:18:12 -0300

    pratica 9: questão 4

--- Pratica9_MonielyBarboza_SilmarPereira.sql
+++ Pratica9_MonielyBarboza_SilmarPereira.sql
@@ -331,3 +331,343 @@ BEGIN
 
     dbms_output.put_line('Federacao ' || v_federacao || ' associada à nacao ' || v_nacao);
 END;
+
+/*4) Implemente as seguines funcionalidades relacionadas ao usuário Cientista do Sistema Sociedade 
+Galática (ver descrição do projeto final):  
+*/
+--a. Gerenciamento:  item 4.a (CRUD de estrelas) 
+CREATE OR REPLACE PACKAGE starManager AS 
+	e_idInvalido EXCEPTION;
+	
+	e_coordsInvalidas EXCEPTION;
+
+	PROCEDURE createStar (
+		p_id ESTRELA.ID_ESTRELA%TYPE,
+		p_name ESTRELA.NOME%TYPE,
+		p_class ESTRELA.CLASSIFICACAO%TYPE,
+		p_massa ESTRELA.MASSA%TYPE,
+		p_x ESTRELA.X%TYPE,
+		p_y ESTRELA.Y%TYPE,
+		p_z ESTRELA.Z%TYPE
+	);
+
+	FUNCTION readStar (p_id ESTRELA.ID_ESTRELA%TYPE) RETURN SYS_REFCURSOR;
+
+	PROCEDURE updateStar (
+		p_idPK ESTRELA.ID_ESTRELA%TYPE,
+		p_name ESTRELA.NOME%TYPE,
+		p_class ESTRELA.CLASSIFICACAO%TYPE,
+		p_massa ESTRELA.MASSA%TYPE,
+		p_x ESTRELA.X%TYPE,
+		p_y ESTRELA.Y%TYPE,
+		p_z ESTRELA.Z%TYPE
+	);
+
+
+	PROCEDURE deleteStar (
+		p_idPK ESTRELA.ID_ESTRELA%TYPE
+	);
+	
+END starManager;
+
+/
+
+CREATE OR REPLACE PACKAGE BODY starManager AS
+	PROCEDURE createStar (
+		p_id ESTRELA.ID_ESTRELA%TYPE,
+		p_name ESTRELA.NOME%TYPE,
+		p_class ESTRELA.CLASSIFICACAO%TYPE,
+		p_massa ESTRELA.MASSA%TYPE,
+		p_x ESTRELA.X%TYPE,
+		p_y ESTRELA.Y%TYPE,
+		p_z ESTRELA.Z%TYPE
+	) IS
+	BEGIN
+		IF p_id IS null THEN
+			raise e_idInvalido;
+		ELSIF p_x IS NULL OR p_y IS NULL OR p_z IS null THEN 
+			raise e_coordsInvalidas;
+		END IF;
+		
+		INSERT INTO ESTRELA VALUES (p_id, p_name, p_class, p_massa, p_x, p_y, p_z);
+	
+		COMMIT;
+	END createStar;
+
+	FUNCTION readStar (p_id ESTRELA.ID_ESTRELA%TYPE) RETURN SYS_REFCURSOR AS
+		c_star SYS_REFCURSOR;
+	BEGIN
+		OPEN c_star FOR
+		SELECT * FROM ESTRELA e
+			WHERE e.ID_ESTRELA = p_id;
+		
+		RETURN c_star;
+	END readStar;
+
+/*
+A aplicação ira exibir ao usuario todas as informações da estrela, o usuario poderá
+modificar qualquer uma exeto o ID, ao chamar por um update será recebida todas as informações
+da estrela, alteradas por usuario ou não alteradas, e o update será feito
+
+Em caso de não haver mudança a ser feira, a aplicação será responsavel por não chamar a função
+*/
+	PROCEDURE updateStar (
+		p_idPK ESTRELA.ID_ESTRELA%TYPE,
+		p_name ESTRELA.NOME%TYPE,
+		p_class ESTRELA.CLASSIFICACAO%TYPE,
+		p_massa ESTRELA.MASSA%TYPE,
+		p_x ESTRELA.X%TYPE,
+		p_y ESTRELA.Y%TYPE,
+		p_z ESTRELA.Z%TYPE
+	) IS
+	BEGIN
+		UPDATE ESTRELA e SET e.NOME = p_name, e.CLASSIFICACAO  = p_class,
+			e.MASSA = p_massa, e.X  = p_x,
+			e.Y  = p_y, e.Z = p_z 
+			WHERE e.ID_ESTRELA = p_idPK; 
+		
+		COMMIT;
+	END updateStar;
+
+
+	PROCEDURE deleteStar (
+		p_idPK ESTRELA.ID_ESTRELA%TYPE
+	)IS
+	BEGIN
+		DELETE FROM ESTRELA e WHERE e.ID_ESTRELA = p_idPK;
+	
+		IF NOT SQL%FOUND THEN
+			RAISE NO_DATA_FOUND;
+		END IF;
+	
+	COMMIT;
+		
+	END deleteStar;
+
+END starManager;
+
+-- Cria estrela valida
+DECLARE 
+	v_id ESTRELA.ID_ESTRELA%TYPE;
+	v_nome ESTRELA.NOME%TYPE;
+	v_class ESTRELA.CLASSIFICACAO%TYPE;
+	v_mass ESTRELA.MASSA%TYPE;
+	v_x ESTRELA.X%TYPE;
+	v_y ESTRELA.Y%TYPE;
+	v_z ESTRELA.Z%TYPE;
+BEGIN
+	v_id := 'C-ar3';
+	v_nome := 'Canopus';
+	v_class := 'P7';
+	v_mass := 5.6454;
+	v_x := 5423;
+	v_y := 532.6566;
+	v_z := 9768.1756;
+
+	starManager.createStar(v_id, v_nome, v_class, v_mass, v_x, v_y, v_z);
+
+	EXCEPTION 
+		WHEN starManager.e_idInvalido THEN
+			dbms_output.put_line('Há problemas no id recebido');
+		WHEN starManager.e_coordsInvalidas THEN
+			dbms_output.put_line('Há problemas nas coordenadas recebidas');
+		WHEN OTHERS THEN  
+			dbms_output.put_line('Erro nro:  ' || SQLCODE  
+      	                     	|| '. Mensagem: ' || SQLERRM );
+END;
+
+SELECT * FROM estrela WHERE ID_ESTRELA = 'C-ar3';
+/*
+C-ar3	Canopus	P7	5.6454	5423	532.6566	9768.1756 
+*/
+
+-- Tenta criar estrela invalida
+DECLARE 
+	v_id ESTRELA.ID_ESTRELA%TYPE;
+	v_nome ESTRELA.NOME%TYPE;
+	v_class ESTRELA.CLASSIFICACAO%TYPE;
+	v_mass ESTRELA.MASSA%TYPE;
+	v_x ESTRELA.X%TYPE;
+	v_y ESTRELA.Y%TYPE;
+	v_z ESTRELA.Z%TYPE;
+BEGIN
+	v_id := 'C-ar3';
+	v_nome := 'Canopus';
+	v_class := 'P7';
+	v_z := 9768.1756;
+
+	starManager.createStar(v_id, v_nome, v_class, v_mass, v_x, v_y, v_z);
+	
+	EXCEPTION 
+		WHEN starManager.e_idInvalido THEN
+			dbms_output.put_line('Há problemas no id recebido');
+		WHEN starManager.e_coordsInvalidas THEN
+			dbms_output.put_line('Há problemas nas coordenadas recebidas');
+		WHEN OTHERS THEN  
+			dbms_output.put_line('Erro nro:  ' || SQLCODE  
+      	                     	|| '. Mensagem: ' || SQLERRM );
+END;
+
+/*
+Há problemas nas coordenadas recebidas
+*/
+
+-- Lê estrela
+DECLARE 
+	c_star SYS_REFCURSOR;
+	v_id ESTRELA.ID_ESTRELA%TYPE;
+
+	v_nome ESTRELA.NOME%TYPE;
+	v_class ESTRELA.CLASSIFICACAO%TYPE;
+	v_mass ESTRELA.MASSA%TYPE;
+	v_x ESTRELA.X%TYPE;
+	v_y ESTRELA.Y%TYPE;
+	v_z ESTRELA.Z%TYPE;
+BEGIN
+	v_id := 'C-ar3';
+	c_star := starManager.readStar(v_id);
+	FETCH c_star INTO
+		v_id,
+		v_nome,
+		v_class,
+		v_mass,
+		v_x,
+		v_y,
+		v_z;
+
+	CLOSE c_star;
+	
+	dbms_output.put_line('ID: '|| v_id || ' Nome: ' || v_nome || 
+		' Classe: ' || v_class || ' Massa: '|| v_mass);
+	
+
+	EXCEPTION 
+		WHEN NO_DATA_FOUND THEN
+			dbms_output.put_line('Não foi possivel encontrar a estrela em questão');
+		WHEN OTHERS THEN  
+			dbms_output.put_line('Erro nro:  ' || SQLCODE  
+      	                     	|| '. Mensagem: ' || SQLERRM );
+END;
+
+/*
+ID: C-ar3 Nome: Canopus Classe: P7 Massa: 5.6454
+*/
+
+--testa update
+DECLARE 
+	v_id ESTRELA.ID_ESTRELA%TYPE;
+	v_nome ESTRELA.NOME%TYPE;
+	v_class ESTRELA.CLASSIFICACAO%TYPE;
+	v_mass ESTRELA.MASSA%TYPE;
+	v_x ESTRELA.X%TYPE;
+	v_y ESTRELA.Y%TYPE;
+	v_z ESTRELA.Z%TYPE;
+BEGIN
+	v_id := 'C-ar3';
+	-- Mudamos o nome
+	v_nome := 'Arakis';
+	v_class := 'P7';
+	v_mass := 5.6454;
+	v_x := 5423;
+	v_y := 532.6566;
+	v_z := 9768.1756;
+	
+	starManager.updateStar(v_id, v_nome, v_class, v_mass, v_x, v_y, v_z);
+END;
+
+SELECT nome FROM estrela WHERE ID_ESTRELA = 'C-ar3';
+/*
+Arakis
+*/
+
+DECLARE
+	v_id ESTRELA.ID_ESTRELA%TYPE;
+
+BEGIN
+	v_id := 'C-ar3';
+	starManager.deleteStar(v_id);
+	EXCEPTION 
+		WHEN NO_DATA_FOUND THEN
+			dbms_output.put_line('Não há o que deletar');
+		WHEN OTHERS THEN  
+			dbms_output.put_line('Erro nro:  ' || SQLCODE  
+      	                     	|| '. Mensagem: ' || SQLERRM );
+END;
+
+SELECT * FROM estrela WHERE ID_ESTRELA = 'C-ar3';
+
+/*Resultado
+
+*/
+
+DECLARE
+	v_id ESTRELA.ID_ESTRELA%TYPE;
+
+BEGIN
+	v_id := 'C-ar3543';
+	starManager.deleteStar(v_id);
+	EXCEPTION 
+		WHEN NO_DATA_FOUND THEN
+			dbms_output.put_line('Não há o que deletar');
+		WHEN OTHERS THEN  
+			dbms_output.put_line('Erro nro:  ' || SQLCODE  
+      	                     	|| '. Mensagem: ' || SQLERRM );
+END;
+/*
+Não há o que deletar
+*/
+
+--b. Relatórios: item 4.a (Informações de Estrelas, Planetas e Sistemas) 
+CREATE OR REPLACE PACKAGE scienceReport AS 
+	FUNCTION systemReport() RETURN SYS_REFCURSOR;
+	FUNCTION planetReport() RETURN SYS_REFCURSOR;
+	FUNCTION starReport() RETURN SYS_REFCURSOR;
+END scienceReport;
+
+/
+
+CREATE OR REPLACE PACKAGE BODY scienceReport AS
+
+/*
+Report contem estrela principal do sistema, estrelas que orbitam a principal, e planetas que orbitam ou
+a principal ou alguma das estrelas orbitantes. 
+*/
+FUNCTION systemReport() RETURN SYS_REFCURSOR AS
+	c_report SYS_REFCURSOR;
+BEGIN 
+	OPEN c_report FOR
+	SELECT s.ESTRELA, s.nome, oe.ORBITANTE, op.PLANETA 
+		FROM SISTEMA s LEFT JOIN ORBITA_ESTRELA oe ON s.ESTRELA = oe.ORBITADA 
+		LEFT JOIN ORBITA_PLANETA op ON op.ESTRELA = s.ESTRELA OR op.ESTRELA = oe.ORBITANTE;
+		
+	RETURN c_report;
+END systemReport();
+
+/*
+ Contem cada planeta e suas informações, e a estrela que o orbita se orbitar
+ Caso não orbite nenhuma estrela na aplicação será dito que é um planeta errante
+ */
+FUNCTION planetReport() RETURN SYS_REFCURSOR AS
+	c_report SYS_REFCURSOR;
+BEGIN 
+	OPEN c_report FOR
+		SELECT p.ID_ASTRO, p.MASSA, p.RAIO, p.CLASSIFICACAO FROM PLANETA p LEFT JOIN ORBITA_PLANETA op ON op.PLANETA = p.ID_ASTRO;
+		
+	RETURN c_report;
+END planetReport();
+
+
+/*
+ Contem informações de cada estrela, e se tem planetas que a orbitam 
+*/
+FUNCTION starReport() RETURN SYS_REFCURSOR AS
+	c_report SYS_REFCURSOR;
+BEGIN 
+	OPEN c_report FOR
+		SELECT e.ID_ESTRELA, e.NOME, e.CLASSIFICACAO, e.MASSA, e.X, e.Y, e.Z, op.PLANETA
+			FROM ESTRELA e LEFT JOIN ORBITA_PLANETA op ON e.ID_ESTRELA  = op.ESTRELA; 
+		
+	RETURN c_report;
+END starReport();
+
+END scienceReport;
